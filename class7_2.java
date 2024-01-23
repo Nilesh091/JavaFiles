9fecb1fe0bbd6a6d4a778bcfb46eae7268d6fce1
Nilesh Ranjan Sahu <nileshranjansahu@Nileshs-MacBook-Air-2.local>
2024-01-23 20:56:18 +0530

    first commit

---
+++ class7_2.java
@@ -0,0 +1,14 @@
+public class class7_2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int a=x&y;
+        System.out.print(a);
+        int b=x|y;
+        System.out.print(b);
+        int c=a^b;
+        System.out.print(c);
+        int d= ~c;
+        System.out.print(d);
+    }
+}
